9dd61a64a954194d1511f4fa0eb91d914f15bbb2
Eric Santana <bitencourteric@hotmail.com>
2024-01-13 21:57:31 -0300

    feat: add nodemon and hot reload

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,4 @@ route.get("/", (req: Request, res: Response) => {
 
 app.use(route);
 
-app.listen(3333, () => "Running on port 3333");
+app.listen(3333, () => console.log("Running on port 3333"));
